8c1b9feb84281f0fed443bd92ff24df87d5d8a5f
Uğur Demir <134082357+ugurdemirogr@users.noreply.github.com>
2023-11-15 17:02:56 +0300

    Update ROC_karsilastirma.py

--- ROC_karsilastirma.py
+++ ROC_karsilastirma.py
@@ -10,9 +10,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-# İlk kod parçası
-data_path = r'C:\Users\ugurd\OneDrive\Masaüstü'
-file_name = 'analiz.xlsx'
+data_path = r'C:\Users\...'  # ' ' işareti olan yere verisetinizin bulunduğu dizini ekleyin.
+file_name = '....xlsx' # ' ' işareti olan yere dosyanızın ismi ve uzantısını yazın.
 full_path = os.path.join(data_path, file_name)
 df = pd.read_excel(full_path)
 
@@ -22,7 +21,7 @@ y = LabelEncoder().fit_transform(df['OAP_Aralıklı'])
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.52, random_state=42)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.38, random_state=42)
 
-model = Sequential([
+model = Sequential([    # Kendi ANN modelinizi tanımlayın.
     Dense(15, input_dim=X_train.shape[1], activation='relu'),
     Dropout(0.5),
     Dense(1, activation='sigmoid')
@@ -30,19 +29,18 @@ model = Sequential([
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 early_stopping = EarlyStopping(monitor='val_loss', patience=10)
 
-history = model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=0)
+history = model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping], verbose=0) #Earlystopping olduğu için 1000 epoch çalışmayabilir.
 y_pred = model.predict(X_test)
 
 fpr_nn, tpr_nn, _ = roc_curve(y_test, y_pred)
 roc_auc_nn = auc(fpr_nn, tpr_nn)
 
 # İkinci kod parçası
-data = pd.read_excel(r'C:\Users\ugurd\OneDrive\Masaüstü\analiz.xlsx')
+data = pd.read_excel(r' ') # ' ' işareti olan yere kendi verisetinizin dizinini yazın.
 
-X = data.iloc[:, 0:8]
-y = data.iloc[:, 8]
-y = y.replace({1: 0, 2: 1})
-X = pd.get_dummies(X, columns=['Cinsiyet_1', 'AnneOgrenim_1', 'BabaOgrenim_1', 'OkumaSiklik_1', 'Internet_1', 'Telefon_1'])
+X = data.iloc[:, 0:8] # giriş verisi
+y = data.iloc[:, 8] # Çıkış verisi
+X = pd.get_dummies(X, columns=['']) #Kategorik değişkenleri belirtebilirsiniz.
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -54,7 +52,7 @@ y_pred_prob_lr = model_lr.predict_proba(X_test)[:, 1]
 fpr_lr, tpr_lr, _ = roc_curve(y_test, y_pred_prob_lr)
 roc_auc_lr = auc(fpr_lr, tpr_lr)
 
-# Tek bir grafikte ROC eğrilerini çizin
+# Tek bir grafikte ROC eğrilerini çizdirilir. ANN ve Regrosyon ROC eğrisi karşılaştırılır.
 plt.figure(figsize=(8, 6))
 plt.plot(fpr_nn, tpr_nn, lw=2, label=f'NN ROC Curve (AUC = {roc_auc_nn:.4f})')
 plt.plot(fpr_lr, tpr_lr, color='darkorange', lw=2, label=f'Logistic Regression ROC Curve (AUC = {roc_auc_lr:.4f})')
